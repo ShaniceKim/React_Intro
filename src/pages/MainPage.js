39a973d83f0e495e566f1dfc2fc3ce6932f2e311
ShaniceKim <qxzo1@naver.com>
2023-10-13 13:56:01 +0900

    feat : modal

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "./MainPage.css";
-import Scrollbar from "smooth-scrollbar";
+import React, { useState, useEffect, useRef } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './MainPage.css';
+import PhotoPage from './PhotoPage';
+
+import Scrollbar from 'smooth-scrollbar';
 
 const cards = [
   {
@@ -23,24 +24,26 @@ const cards = [
     description: (
       <Typography variant="caption">당신의 기분을 보여주세요!</Typography>
     ),
-    imageUrl: "https://source.unsplash.com/random?wallpapers",
+    imageUrl: 'https://source.unsplash.com/random?wallpapers',
   },
   {
     id: 2,
     title: <Typography variant="body2">Emotion Sparring Room</Typography>,
     description: <Typography variant="caption">감정 스파링</Typography>,
-    imageUrl: "https://source.unsplash.com/random?nature",
+    imageUrl: 'https://source.unsplash.com/random?nature',
   },
 ];
 
 const defaultTheme = createTheme();
 
 export default function Album() {
-  const [text, setText] = useState("");
-  const [subText, setSubText] = useState("");
-  const initialText = "Mood Canvas";
-  const subInitialText = "당신의 감정을 얼굴로 표현해보세요 !";
+  const [text, setText] = useState('');
+  const [subText, setSubText] = useState('');
+  const initialText = 'Mood Canvas';
+  const subInitialText = '당신의 감정을 얼굴로 표현해보세요 !';
   const [hoveredCard, setHoveredCard] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false); // Added state for modal
+
   const typingSpeed = 100;
 
   const typeText = (text, setText, initialText) => {
@@ -67,12 +70,20 @@ export default function Album() {
     }, typingSpeed);
   };
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   useEffect(() => {
     typeText(text, setText, initialText);
 
     // When "Mood Canvas" typing animation is finished
     setTimeout(() => {
-      setText(""); // Clear the text for the new typing animation
+      setText(''); // Clear the text for the new typing animation
       typeSubText(subText, setSubText, subInitialText);
     }, initialText.length * typingSpeed + 500); // Adding a delay of 500ms after "Mood Canvas" typing animation
   }, []);
@@ -91,7 +102,7 @@ export default function Album() {
       <main ref={mainRef}>
         <Box
           sx={{
-            bgcolor: "background.paper",
+            bgcolor: 'background.paper',
             pt: 8,
             pb: 6,
           }}
@@ -129,15 +140,16 @@ export default function Album() {
           <Grid container spacing={4} justifyContent="center">
             {cards.map((card) => (
               <Grid item key={card.id} xs={12} sm={6} md={4}>
-                <Link to="/photo">
+                {/* Added onClick to open modal */}
+                <div onClick={openModal}>
                   <Card
                     sx={{
-                      height: "100%",
-                      display: "flex",
-                      flexDirection: "column",
-                      transition: "transform 0.3s",
+                      height: '100%',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      transition: 'transform 0.3s',
                       transform:
-                        hoveredCard === card.id ? "scale(1.2)" : "scale(1)",
+                        hoveredCard === card.id ? 'scale(1.2)' : 'scale(1)',
                     }}
                     onMouseEnter={() => setHoveredCard(card.id)}
                     onMouseLeave={() => setHoveredCard(null)}
@@ -145,34 +157,33 @@ export default function Album() {
                     <CardMedia
                       component="div"
                       sx={{
-                        pt: "56.25%",
+                        pt: '56.25%',
                       }}
                       image={card.imageUrl}
                     />
-                    <CardContent sx={{ flexGrow: 1, textAlign: "center" }}>
+                    <CardContent sx={{ flexGrow: 1, textAlign: 'center' }}>
                       <Typography gutterBottom variant="h5" component="h2">
                         {card.title}
                       </Typography>
                       <Typography>{card.description}</Typography>
                     </CardContent>
                   </Card>
-                </Link>
+                </div>
               </Grid>
             ))}
           </Grid>
         </Container>
       </main>
-      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom></Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          MoodCanvas@2023
-        </Typography>
-      </Box>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>모달</h2>
+          </div>
+        </div>
+      )}
     </ThemeProvider>
   );
 }
